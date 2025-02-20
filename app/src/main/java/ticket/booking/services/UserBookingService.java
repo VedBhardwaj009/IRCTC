ba39afa751be09a1c8f00911688cd44d8a713a87
Ved Bhardwaj <vedpandit5252@gmail.com>
2025-02-20 17:03:04 +0530

    MAKING AN BACKEND APPLICATION FOR BOOKING TICKETS LIKE IRCTC

---
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -0,0 +1,84 @@
+package ticket.booking.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.User;
+import ticket.booking.entities.Ticket;
+import ticket.booking.util.UserServiceUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class UserBookingService {
+
+    private User user;
+
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    public UserBookingService(User user1) throws IOException {
+        this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        });
+    }
+
+    public Boolean loginUser() {
+        Optional<User> foundUser = userList.stream()
+                .filter(user1 -> {
+                    return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+                })
+                .findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1) {
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File userFile = new File(USERS_PATH);
+        objectMapper.writeValue(userFile, userList);
+    }
+
+    public void fetchBooking() {
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the ticket id to cancel");
+        ticketId = s.next();
+
+        if (ticketId == null || ticketId.isEmpty()) {
+            System.out.println("Ticket ID cannot be null or empty.");
+            return Boolean.FALSE;
+        }
+
+        String finalTicketId1 = ticketId;  //Because strings are immutable
+        boolean removed = user.getTicketsBooked().removeIf(ticket -> ticket.getTicketId().equals(finalTicketId1));
+
+        String finalTicketId = ticketId;
+        user.getTicketsBooked().removeIf(Ticket -> Ticket.getTicketId().equals(finalTicketId));
+        if (removed) {
+            System.out.println("Ticket with ID " + ticketId + " has been canceled.");
+            return Boolean.TRUE;
+        } else {
+            System.out.println("No ticket found with ID " + ticketId);
+            return Boolean.FALSE;
+        }
+    }
+}
